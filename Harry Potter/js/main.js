1a2047d8949686c529a99e84d072a34ee0b6df8a
Erick Clarivet <erick.clarivet@gmail.com>
2020-11-30 15:06:32 +0100

    Adding main features Bootstrap and templates

---
+++ Harry Potter/js/main.js
@@ -0,0 +1,8 @@
+/* https://www.w3schools.com/bootstrap/bootstrap_popover.asp */
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
